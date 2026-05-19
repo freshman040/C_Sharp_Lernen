b51646ae38fc51de39030c7e1d43120bd315913a
freshman040 <intersoft123@gmx.de>
2026-05-19 17:53:26 +0200

    Einlesen und Auslesen in eine Datei

--- ReadingAndWritingFiles/Program.cs
+++ ReadingAndWritingFiles/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:\\Users\\TAKE-IT\\Documents\\Csharp\\example.txt";
+
+        // Writing to a file
+        //string content = "Hello, World!";
+        //File.WriteAllText(filePath, content);
+
+        // Reading from a file
+        string content = File.ReadAllText(filePath);
+        Console.WriteLine(content);
+
+
+
+    }
+}
