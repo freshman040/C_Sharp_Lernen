1a364dae81109a4445fb1fad36af9bc981334841
freshman040 <intersoft123@gmx.de>
2026-05-19 15:14:00 +0200

    Aenderungen nach Ausfuehrung

--- TryCatch/TryCatch/obj/Debug/net8.0/TryCatch.AssemblyInfo.cs
+++ TryCatch/TryCatch/obj/Debug/net8.0/TryCatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TryCatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f27de493ab36650a28d490253e9e0ec81f6777d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TryCatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TryCatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
